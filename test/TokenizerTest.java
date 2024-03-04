0e2170d38fca45b713131d5b63bb079f08a52873
Paul Chen <paulchen95@gmail.com>
2024-03-03 22:18:14 -0600

    update test

--- test/TokenizerTest.java
+++ test/TokenizerTest.java
@@ -86,6 +86,71 @@ public class TokenizerTest {
         }
     }
 
+    @Test
+    public void TestViewExpressionParan() {
+        String test = "view (3 + 5)";
+        List<Token> expected = new ArrayList<Token>();
+        expected.add(new Token(TokenType.VIEW));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.NUM, 3));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.NUM, 5));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        String error = assertTokenizer(test, expected);
+        if (error != null) {
+            assertTrue(error, false);
+        }
+    }
+
+    @Test
+    public void TestViewExpressionComplex() {
+        String test = "view ((3 + 5) + (3 + 5))";
+        List<Token> expected = new ArrayList<Token>();
+        expected.add(new Token(TokenType.VIEW));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.NUM, 3));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.NUM, 5));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.NUM, 3));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.NUM, 5));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        String error = assertTokenizer(test, expected);
+        if (error != null) {
+            assertTrue(error, false);
+        }
+    }
+
+    @Test
+    public void TestViewExpressionComplex2() {
+        String test = "view ((x+5)+y*(5*z))";
+        List<Token> expected = new ArrayList<Token>();
+        expected.add(new Token(TokenType.VIEW));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.VAR, "x"));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.NUM, 5));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        expected.add(new Token(TokenType.PLUS));
+        expected.add(new Token(TokenType.VAR, "y"));
+        expected.add(new Token(TokenType.STAR));
+        expected.add(new Token(TokenType.LEFT_PARAN));
+        expected.add(new Token(TokenType.NUM, 5));
+        expected.add(new Token(TokenType.STAR));
+        expected.add(new Token(TokenType.VAR, "z"));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        expected.add(new Token(TokenType.RIGHT_PARAN));
+        String error = assertTokenizer(test, expected);
+        if (error != null) {
+            assertTrue(error, false);
+        }
+    }
 
     String assertTokenizer(String test, List<Token> expected) {
         String error = null;
