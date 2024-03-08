fc6819c401f0c10b4bc6fe2a9de132581b4fae0b
David <chen7790@umn.edu>
2024-03-07 19:55:30 -0600

    view node

---
+++ src/com/chen/mint/Nodes/ViewNode.java
@@ -0,0 +1,16 @@
+package com.chen.mint.Nodes;
+
+import java.util.List;
+
+import com.chen.mint.Token;
+import com.chen.mint.TokenType;
+
+public class ViewNode extends Node{
+    
+    public ViewNode(List<Token> input){
+        if(input.get(0).type.equals(TokenType.VIEW)){
+            input.remove(0);
+            this.left = new ExpNode(input);
+        }
+    }
+}
