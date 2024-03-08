177d8888bb1b24ebb66227f8564adf4b80338310
Paul Chen <paulchen95@gmail.com>
2024-03-07 20:15:51 -0600

    simple let test

--- test/ParserTest.java
+++ test/ParserTest.java
@@ -12,7 +12,9 @@ import com.chen.mint.Parser;
 import com.chen.mint.Token;
 import com.chen.mint.TokenType;
 import com.chen.mint.Nodes.ExpNode;
+import com.chen.mint.Nodes.LetNode;
 import com.chen.mint.Nodes.Node;
+import com.chen.mint.Nodes.VarNode;
 import com.chen.mint.Nodes.ViewNode;
 
 public class ParserTest {
@@ -38,6 +40,38 @@ public class ParserTest {
         Assert.assertTrue("root->left must be ExpNode", (exp instanceof ExpNode));
         Assert.assertEquals("root->left->num must be 3", 3, exp.num);
         Assert.assertNull("root->left->varName must be null", exp.varName);
+    }
+
+    @Test
+    public void TestParseLetSimple() {
+        //SETUP
+        //input = "let x = 3";
+        List<Token> input = new ArrayList<Token>();
+        input.add(new Token(TokenType.LET));
+        input.add(new Token(TokenType.VAR, "x"));
+        input.add(new Token(TokenType.EQUAL));
+        input.add(new Token(TokenType.NUM, 3));
+
+        //TEST
+        Node let = Parser.parse(input);
+
+        //VERIFY
+        Assert.assertNotNull("let must not be null", let);
+        Assert.assertTrue("let must be ViewNode", (let instanceof LetNode));
+        Assert.assertNotNull("let->left must not be null", let.left);
+        Assert.assertNotNull("let->right must not be null", let.right);
+        Node var = let.left;
+        Assert.assertNull("let->left->left must be null", var.left);
+        Assert.assertNull("let->left->right must be null", var.right);
+        Assert.assertTrue("let->left must be VarNode", (var instanceof VarNode));
+        Assert.assertEquals("let->left->num must be 0", 0, var.num);
+        Assert.assertEquals("let->left->varName must be x", "x", var.varName);
+        Node exp = let.right;
+        Assert.assertNull("let->left->left must be null", exp.left);
+        Assert.assertNull("let->left->right must be null", exp.right);
+        Assert.assertTrue("let->left must be ExpNode", (exp instanceof ExpNode));
+        Assert.assertEquals("let->left->num must be 3", 3, exp.num);
+        Assert.assertNull("let->left->varName must be null", exp.varName);
     }
 
 }
